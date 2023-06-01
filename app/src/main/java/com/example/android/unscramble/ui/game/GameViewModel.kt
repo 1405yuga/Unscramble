76b3838837a4f8ba9c7de66cff700045780702a2
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-06-01 21:40:04 +0530

    studying lifecycle of viewModel

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -1,12 +1,27 @@
 package com.example.android.unscramble.ui.game
 
+
+import android.util.Log
 import androidx.lifecycle.ViewModel
 
+val TAG = "Game Fragment"
+
 class GameViewModel : ViewModel() {
-    private var currentWordcount =0
-    private var score =0
+    private var currentWordcount = 0
+    private var score = 0
 
     //backing property
-    private var _currentScrambleWord ="test"
-    val currentSrambleWord : String get() = _currentScrambleWord
+    private var _currentScrambleWord = "test"
+    val currentScrambleWord: String get() = _currentScrambleWord
+
+    //initializer block
+    init {
+        Log.d(TAG, "GameViewModel initiated")
+    }
+
+    //called when view model destroyed
+    override fun onCleared() {
+        super.onCleared()
+        Log.d(TAG, "GameViewModel destroyed!!!")
+    }
 }
