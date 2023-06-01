27b4fbfc689dbb8b8700e5148ea884c38f928aec
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-06-01 20:25:06 +0530

    creating view Model

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -3,4 +3,7 @@ package com.example.android.unscramble.ui.game
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
+    private var currentWordcount =0
+    private var score =0
+    private var currentWord ="test"
 }
