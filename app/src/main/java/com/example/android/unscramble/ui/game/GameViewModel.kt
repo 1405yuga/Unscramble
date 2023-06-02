5cd413ffe76f2df7aa01182928f1e9063beb4ccf
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-06-02 12:55:01 +0530

    ViewModel destroy is working

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -9,10 +9,40 @@ val TAG = "Game Fragment"
 class GameViewModel : ViewModel() {
     private var currentWordcount = 0
     private var score = 0
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
 
     //backing property
-    private var _currentScrambleWord = "test"
+    private  var _currentScrambleWord ="test"
     val currentScrambleWord: String get() = _currentScrambleWord
+
+    private fun getNextWord() {
+        Log.d(TAG, "getNextWord called!!")
+
+
+        do {
+            currentWord = allWordsList.random()
+        } while (!wordsList.contains(currentWord))
+        val tempWord = currentWord.toCharArray()
+
+        //shuffle the words until it is not same as current word
+        while (!tempWord.toString().equals(currentWord)) {
+            tempWord.shuffle()
+        }
+
+        //check if it already used word
+        _currentScrambleWord = String(tempWord)
+        ++currentWordcount
+        wordsList.add(currentWord)
+
+    }
+
+    private fun nextWord(): Boolean {
+        return if (currentWordcount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
+    }
 
     //initializer block
     init {
@@ -24,4 +54,6 @@ class GameViewModel : ViewModel() {
         super.onCleared()
         Log.d(TAG, "GameViewModel destroyed!!!")
     }
+
+
 }
